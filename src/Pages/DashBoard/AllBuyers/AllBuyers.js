489f2cf3e0ec06383be8250b3ccd10183bf83221
saruaralam-swd <saruaralam2001@gmail.com>
2022-11-27 00:46:09 +0600

    load all buyers

--- src/Pages/DashBoard/AllBuyers/AllBuyers.js
+++ src/Pages/DashBoard/AllBuyers/AllBuyers.js
@@ -1,9 +1,54 @@
-import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import React, { useContext } from 'react';
+import Loading from '../../../Components/Loading';
+import { AuthContext } from '../../../Context/AuthProvider';
+import { CheckBadgeIcon, TrashIcon } from '@heroicons/react/24/solid'
 
 const AllBuyers = () => {
+  const { user } = useContext(AuthContext);
+
+  const { data: buyers = [], isLoading } = useQuery({
+    queryKey: ['allBuyers', user?.email],
+    queryFn: async () => {
+      const res = await fetch(`http://localhost:5000/allBuyers?email=${user?.email}`);
+      const data = await res.json();
+      return data;
+    }
+  })
+
+  if (isLoading) {
+    return <Loading></Loading>
+  }
+
+  console.log(buyers);
+
   return (
     <div>
       <h2 className="text-3xl text-center">All Buyers</h2>
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+
+          <thead>
+            <tr>
+              <th>S/N</th>
+              <th>Name</th>
+              <th>delete</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {
+              buyers?.map((buyer, index) =>
+                <tr key={index}>
+                  <th>{index + 1}</th>
+                  <td>{buyer?.email}</td>
+                  <td><button><TrashIcon className='h-10 w-10 text-red-400' /></button></td>
+                </tr>
+              )
+            }
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
--- src/Pages/DashBoard/AllSellers/AllSellers.js
+++ src/Pages/DashBoard/AllSellers/AllSellers.js
@@ -1,9 +1,65 @@
-import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import React, { useContext } from 'react';
+import Loading from '../../../Components/Loading';
+import { AuthContext } from '../../../Context/AuthProvider';
+import { CheckBadgeIcon, TrashIcon } from '@heroicons/react/24/solid'
 
 const AllSellers = () => {
+  const { user } = useContext(AuthContext);
+
+  const { data: sellers = [], isLoading } = useQuery({
+    queryKey: ['mySellers', user?.email],
+    queryFn: async () => {
+      const res = await fetch(`http://localhost:5000/mySellers?email=${user?.email}`);
+      const data = await res.json();
+      return data;
+    }
+  })
+
+  if (isLoading) {
+    return <Loading></Loading>
+  }
+
+  console.log(sellers);
+
   return (
     <div>
       <h2 className="text-3xl text-center">All Sellers</h2>
+
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+
+          <thead>
+            <tr>
+              <th>S/N</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>verify</th>
+              <th>delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              sellers?.map((seller, index) =>
+                <tr key={index}>
+                  <th>{index + 1}</th>
+                  <td>{seller?.sellerName}</td>
+                  <td>{seller?.sellerEmail}</td>
+                  <td>
+                    {
+                      seller?.verify === "unverified" && <button className='btn btn-secondary btn-sm'>make verify</button>
+                    }
+                    {
+                      seller?.verify === "verified" && <button><CheckBadgeIcon className='h-6 w-6 text-green-500' /> </button>
+                    }
+                  </td>
+                  <td><button><TrashIcon className='h-10 w-10 text-red-400' /></button></td>
+                </tr>
+              )
+            }
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
