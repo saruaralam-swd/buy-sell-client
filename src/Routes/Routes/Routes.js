1200a26607b70ebd955d1e5bf24f9b11fbd080f1
saruaralam-swd <saruaralam2001@gmail.com>
2022-11-27 13:02:24 +0600

    protected admin and seller route

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -14,6 +14,7 @@ import Login from "../../Pages/Login/Login";
 import SignUp from "../../Pages/SignUp/SignUp";
 import AdminRoute from "../AdminRoute/AdminRoute";
 import PrivateRoute from "../PrivateRoute/PrivateRoute";
+import SellerRoute from "../SellerRoute/SellerRoute";
 
 export const router = createBrowserRouter([
   {
@@ -49,15 +50,15 @@ export const router = createBrowserRouter([
       },
       {
         path: '/dashboard/myProducts',
-        element: <MyProducts></MyProducts>
+        element: <SellerRoute><MyProducts></MyProducts></SellerRoute>
       },
       {
         path: '/dashboard/addProducts',
-        element: <AddProduct></AddProduct>
+        element: <SellerRoute><AddProduct></AddProduct></SellerRoute>
       },
       {
         path: '/dashboard/myBuyer',
-        element: <MyBuyers></MyBuyers>
+        element: <SellerRoute><MyBuyers></MyBuyers></SellerRoute>
       },
       {
         path: '/dashboard/allSeller',
--- src/hooks/UseSeller.js
+++ src/hooks/UseSeller.js
@@ -6,7 +6,7 @@ const useSeller = email => {
 
   useEffect(() => {
     if (email) {
-      fetch(`http://localhost:5000/seller/${email}`)
+      fetch(`http://localhost:5000/user/seller/${email}`)
         .then(res => res.json())
         .then(data => {
           console.log(data);
