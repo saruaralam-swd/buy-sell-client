ba7ddc7d3e5621bbcd873808fdbc3659ff9a211d
saruaralam-swd <saruaralam2001@gmail.com>
2023-02-05 00:50:10 +0600

    browse categories section and add font family

--- src/Pages/Home/Advertisement.js/AdvertisementCard.js
+++ src/Pages/Home/Advertisement.js/AdvertisementCard.js
@@ -29,17 +29,15 @@ const AdvertisementCard = ({ product, setProduct }) => {
         <div className='col-span-4 md:col-span-2 lg:col-span-3 grid sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-5 pb-10 p-5'>
           <div className='space-y-1 md:border-r-2 md:pr-2'>
             <h2 className='text-2xl font-semibold'> {productName} </h2>
-    
-            
             <div className='flex gap-10 font-semibold'>
               <p className='text-blue-500 font-bold text-2xl'>{resalePrice} TK</p>
               <del>{originalPrice}Tk</del>
             </div>
-
             <p>{description.length > 100 ? (description.slice(0, 100) + "...") : description}</p>
-            <div className=''>
+            <div>
               <label onClick={() => setProduct(product)} htmlFor="product-order-modal" className="btn btn-primary btn-sm mr-2"> Book Now </label>
-              <button onClick={() => handleReportToAdmin(_id)} className=' mt-2 md:mt-0 border rounded-md px-4 py-1 bg-red-300 text-black font-semibold'>Report To Admin</button>
+              <button onClick={() => handleReportToAdmin(_id)} className='mt-2 md:mt-0 border rounded-md px-4 py-1 bg-red-300 text-black font-semibold'>Report To Admin</button>
+              <button className='mt-2 md:mt-0 border rounded-md px-4 py-1 bg-red-300 text-black font-semibold'>Details</button>
             </div>
           </div>
 
--- src/Pages/Home/Categories/Categories.js
+++ src/Pages/Home/Categories/Categories.js
@@ -12,60 +12,59 @@ const Categories = () => {
   const { categories } = useContext(AuthContext);
 
   return (
-    <>
-      <div className='my-10 px-5'>
-        <h2 className='text-2xl mb-3 font-semibold'>Browse Categories</h2>
-        <Swiper
-          style={{
-            "--swiper-navigation-size": "22px",
-          }}
-          breakpoints={{
-            640: {
-              width: 640,
-              slidesPerView: 3,
-            },
-            768: {
-              width: 640,
-              slidesPerView: 3,
-            },
-            1024: {
-              width: 1024,
-              slidesPerView: 3,
-            },
-          }}
-          slidesPerView={1}
-          spaceBetween={10}
-          freeMode={true}
-          loop={true}
-          keyboard={{
-            enabled: true,
-          }}
-          autoplay={{
-            delay: 3000,
-            disableOnInteraction: false,
-          }}
-          navigation={true}
-          modules={[Autoplay, Navigation, FreeMode, Keyboard]}
-          className="w-full h-[230px] border rounded-md swiper"
-        >
-          <div>
-            {
-              categories.map(category =>
-                <SwiperSlide className='text-center hover:underline'>
-                  <Link to={`/category/${category?._id}`} >
-                    <div>
-                      {/* <img src={category?.img} className='' alt="category img" /> */}
-                      <img  src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsip1U_U5ElDOrtQiAGbVgAmV-LU2GaUFRew&usqp=CAU' className='object-cover mx-auto' alt="category img" />
-                      <p>{category?.categoryName}</p>
-                    </div>
-                  </Link>
-                </SwiperSlide>
-              )
-            }
-          </div>
-        </Swiper>
-      </div>
-    </>
+    <div className='my-10 mx-5 bg-white rounded-md'>
+      <h2 className='text-xl font-bold border-b-2 pb-2 p-2'>Browse Categories</h2>
+      <Swiper
+        style={{
+          "--swiper-navigation-size": "12px",
+        }}
+        breakpoints={{
+          640: {
+            width: 640,
+            slidesPerView: 3,
+          },
+          768: {
+            width: 640,
+            slidesPerView: 3,
+          },
+          1024: {
+            width: 1024,
+            slidesPerView: 3,
+          },
+        }}
+        slidesPerView={1}
+        spaceBetween={10}
+        freeMode={true}
+        loop={true}
+        keyboard={{
+          enabled: true,
+        }}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
+        navigation={true}
+        modules={[Autoplay, Navigation, FreeMode, Keyboard]}
+        className="w-full h-[180px] swiper mt-2"
+      >
+        <div>
+          {
+            categories.map(category =>
+              <SwiperSlide className='text-center hover:underline'>
+                <Link to={`/category/${category?._id}`} >
+                  <div>
+                    {/* <img src={category?.img} className='' alt="category img" /> */}
+                    <img className='w-32 h-32 object-cover mx-auto' src='https://media.istockphoto.com/id/1161116588/photo/mobile-phone-top-view-with-white-screen.jpg?s=612x612&w=0&k=20&c=6nGTbnTvQUiq2XXSYuT411pC-5B1SUjhpLhE3eGrNIw=' alt="category img" />
+                    <p>{category?.categoryName}</p>
+                  </div>
+                </Link>
+              </SwiperSlide>
+            )
+          }
+        </div>
+      </Swiper>
+    </div>
+
   );
 };
 
