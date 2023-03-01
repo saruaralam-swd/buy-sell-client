c5246397f703db3d29641840ba4e7e68538cb0fc
saruaralam-swd <saruaralam2001@gmail.com>
2023-03-01 23:56:05 +0600

    price sorting feature add

--- src/Pages/Home/Advertisement.js/Advertisement.js
+++ src/Pages/Home/Advertisement.js/Advertisement.js
@@ -7,11 +7,13 @@ import AdvertisementCard from './AdvertisementCard';
 import { BsFillGrid3X3GapFill } from "react-icons/bs";
 import { FaListUl } from "react-icons/fa";
 import ProductGridView from './ProductGridView';
+import sortImg from '../../../assets/image/sort.png';
 
 const Advertisement = () => {
   const { user } = useContext(AuthContext);
   const [product, setProduct] = useState(null);
   const [productView, setProductView] = useState('');
+  const [isAsc, setIsAsc] = useState('');
 
   const { data: advertisement = [], isLoading } = useQuery({
     queryKey: ['advertisement'],
@@ -26,8 +28,16 @@ const Advertisement = () => {
     }
   });
 
+
   if (isLoading) {
     return <Loading></Loading>
+  }
+
+  if (isAsc === 'Low Price') {
+    advertisement.sort(function (a, b) { return a.resalePrice - b.resalePrice });
+  }
+  if (isAsc === 'High Price') {
+    advertisement.sort(function (a, b) { return b.resalePrice - a.resalePrice });
   }
 
   return (
@@ -35,10 +45,18 @@ const Advertisement = () => {
       {
         advertisement.length > 0 &&
         <div className='px-10' >
-          <div className='flex items-center justify-between'>
-            <div className='hidden lg:block'>{" "}</div>
+          <div className=''>
             <h2 className="text-2xl text-center font-bold text-slate-700 mb-10">Advertisement</h2>
-            <div className='flex gap-3'>
+
+            <div className='mb-5 flex items-center gap-2'>
+              <select onChange={(e) => setIsAsc(e.target.value)} className="select select-bordered select-sm w-56 focus:outline-none">
+                <option disabled selected>-- Price --</option>
+                <option>Low Price</option>
+                <option>High Price</option>
+              </select>
+
+              <img src={sortImg} alt="" />
+
               <BsFillGrid3X3GapFill onClick={() => setProductView(true)} className='h-6 w-6 inline-block cursor-pointer' />
               <FaListUl onClick={() => setProductView(false)} className='w-6 h-6 inline-block cursor-pointer' />
             </div>
--- src/Pages/Home/Advertisement.js/ProductGridView.js
+++ src/Pages/Home/Advertisement.js/ProductGridView.js
@@ -7,10 +7,10 @@ const ProductGridView = ({ product, setProduct, setSellerInfo }) => {
     <div className='border rounded-lg border-slate-100 shadow-lg hover:shadow-2xl duration-500'>
       <img className='md:h-[280px] lg:h-[200px] mx-auto my-5' src={image} alt="" />
       <div className='p-5'>
-        <h2>{productName}</h2>
+        <h2 className='text-2xl'>{productName}</h2>
         <div className='flex items-center gap-5 my-1'>
           <p className='text-blue-500 font-bold text-xl'>{resalePrice} TK</p>
-          <p className='text-slate-600'><del>{originalPrice}</del> TK</p>
+          <p className='text-slate-600'><del>{originalPrice} TK</del></p>
         </div>
         <label onClick={() => setProduct(product)} htmlFor="product-order-modal" className="btn btn-primary btn-sm mr-2"> Book Now </label>
       </div>
